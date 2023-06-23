f188416f9ba24b99448168b41e2a25b4bd2957bd
dcnis <bgk.dennis@yahoo.de>
2023-06-23 23:37:27 +0800

    created mvn project

---
+++ src/main/java/de/schmidtdennis/validatorpattern/ValidatorPatternApplication.java
@@ -0,0 +1,13 @@
+package de.schmidtdennis.validatorpattern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValidatorPatternApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ValidatorPatternApplication.class, args);
+	}
+
+}
